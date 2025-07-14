a4f11037531570ae2a7dd5c638edee01e1d58f88
YashasviAkhilesham <yboiz238@gmail.com>
2025-07-14 19:51:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 
-const VOICEFLOW_API_KEY = 'Bearer VF.DM.686946d7d3fd6eed7a8f9abc.ibWdbRLFF02N5vud'; // Replace if needed
+const VOICEFLOW_API_KEY = 'Bearer VF.DM.686946d7d3fd6eed7a8f9abc.ibWdbRLFF02N5vud'; 
 
 app.post('/whatsapp', async (req, res) => {
   const msg = req.body.Body;
@@ -45,6 +45,9 @@ app.post('/whatsapp', async (req, res) => {
     res.set('Content-Type', 'text/xml');
     res.send(twiml.toString());
   }
+});
+app.get('/', (req, res) => {
+  res.send('Bot is deployed and running!');
 });
 
 app.listen(3000, () => {
